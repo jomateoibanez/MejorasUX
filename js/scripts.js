71fa1e3b5a936310d593f298525d14a677d5f9a4
Jomateoibanez <82480328+jomateoibanez@users.noreply.github.com>
2022-05-04 22:17:14 -0400

    undefined

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var cont = document.getElementById("asd");
 
 if (sessionStorage.font !== null) {
-    if (!a) {
+    if (a == undefined) {
         a = 62.5 + '%';
         cont.style.fontSize = a;
     } else {
